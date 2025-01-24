aadeb511ba45f236dca01a159e4c7f4ea796ee27
Logunathan007 <165123698+masini-idss@users.noreply.github.com>
2025-01-24 15:26:20 +0530

    Add project files.

---
+++ FileToBase64Convertor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClipboardExample
+{
+    class Program
+    {
+        [STAThread] // This ensures the main thread runs in Single Threaded Apartment (STA) mode
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\Logu\\XML\\HPXL\\xml\\custom.xml";
+
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+
+            string base64String = Convert.ToBase64String(fileBytes);
+
+            Clipboard.SetText(base64String);
+
+            Console.WriteLine("Text copied to clipboard!");
+        }
+    }
+}
